6d0f369d7a70b071c039cd1f018c3a7b9ab7f186
Thierry Beeckmans <thierry.beeckmans@gmail.com>
2016-09-17 09:30:43 +0200

    css adjustment logo banner

--- themes/bht_theme/templates/sponsor/node--logo.tpl.php
+++ themes/bht_theme/templates/sponsor/node--logo.tpl.php
@@ -4,6 +4,9 @@ if (isset($node->attributes_array) && is_array($node->attributes_array) ) {
   $attributes_array = drupal_array_merge_deep($attributes_array, $node->attributes_array);
 }
 $attributes_array['class'][] = 'sponsor__item';
+if ($sticky) {
+  $attributes_array['class'][] = 'sticky';
+}
 ?>
 
 <div <?php print drupal_attributes($attributes_array); ?>>
